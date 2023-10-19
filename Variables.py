8e8bada8bb854cbc14a883ae0413865a8c17e69c
Akhil Verma <70507574+akhilverma5007@users.noreply.github.com>
2023-10-19 19:29:38 +0530

    Day 1

---
+++ Variables.py
@@ -0,0 +1,12 @@
+name = ("Hello whats your name")
+print(name)
+
+num = 32424
+print(num)
+
+name = "Akhil"
+print(name)
+
+userInput = input("What's Your Name? ")
+length = len(userInput)
+print(length)
---
+++ main.py
@@ -0,0 +1,22 @@
+#print Function
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Strings
+print("Hello World!\nHello World!\nHello World!")
+
+#String Concatenation (Combining different Strings together)
+print("Hello" + "World")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Input Function
+input("What is your name? ")
+#Concatinated
+print("Hello " + input("How are you? "))
+
+#To print length of input
+print(len(input("")))
