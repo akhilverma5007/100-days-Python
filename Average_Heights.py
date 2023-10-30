bfb6a0a6d74d5aaf1e03b00f3f30775cfce68b46
Akhil Verma <70507574+akhilverma5007@users.noreply.github.com>
2023-10-30 15:07:06 +0530

    final commit

---
+++ Average_Heights.py
@@ -0,0 +1,3 @@
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
---
+++ Loops.py
@@ -0,0 +1,4 @@
+# For Loops
+tempList = ["Akhil", "Rohit", "Sanchit", "Muskan", "Mukdar"]
+for each in tempList:
+    print(each)
